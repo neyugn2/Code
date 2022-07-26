e183346e028db559b1404ae3638fc60af822545f
neyugn2 <trannguyenvt2006@gmail.com>
2022-07-26 14:25:37 +0700

    commit

--- a.cpp
+++ a.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
 #define fast() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define int long long
+#define endl "\n"
 
 using namespace std;
+
+void input() {
+
+}
 
 void solve() {
 
 }
 
 int32_t main() {
-    freopen("a.inp", "r", stdin);
-    freopen("a.out", "w", stdout);
     fast();
+    input();
     solve();
     return 0;
 }
