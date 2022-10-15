00e9145221b55eca47e8ae81e3453faa13cfae85
neyugn <neyugn@pop-os.localdomain>
2022-10-15 20:24:55 +0700

    .

---
+++ Train/xephang/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define fast() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define int long long
+#define endl "\n"
+
+using namespace std;
+
+int n, a[50];
+
+void solve() {
+}
+
+int32_t main() {
+    fast();
+    solve();
+    return 0;
+}
+
