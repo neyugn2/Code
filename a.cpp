fd31ac2ff3d430e3a6d5908f993c3ed6b0a6ce9b
neyugn <neyugn@pop-os.localdomain>
2022-11-13 20:33:03 +0700

    committed

--- a.cpp
+++ a.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define fast() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define fileInput(NAME) freopen(NAME".inp", "r", stdin); freopen(NAME".out", "w", stdout);
 #define int long long
 #define endl "\n"
 
@@ -11,6 +12,7 @@ void solve() {
 
 int32_t main() {
     fast();
+    //fileInput("a");
     solve();
     return 0;
 }
